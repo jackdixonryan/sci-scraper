c27af00ca90728f77979e7cf3a4605124535f34d
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-10-14 18:14:51 -0600

    Troubling shoot Deploy

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // cheerio needs to look for class cIugan to get reddit text.
 
 app.get('/api/:ology', (req, response) => {
